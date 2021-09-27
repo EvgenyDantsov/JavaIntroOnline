acd97b9c375e1e19a90773941e0dccf530e346f5
EvgenyDantsov <adriano-sport@mail.ru>
2021-09-27 22:36:02 +0300

    add all task branching

--- UnitOne/src/main/java/com/evgeny/unit/branching/TaskFiveBranch.java
+++ UnitOne/src/main/java/com/evgeny/unit/branching/TaskFiveBranch.java
@@ -1,4 +1,15 @@
 package com.evgeny.unit.branching;
 
+import java.util.Scanner;
+
 public class TaskFiveBranch {
+    public static void main(String[] args) {
+        int x;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите x: ");
+        x = in.nextInt();
+        if (x <= 3) {
+            System.out.println("f: " + (Math.pow(x, 2) - 3 * x + 9));
+        } else System.out.println("f: " + (1 / (Math.pow(x, 3) + 6)));
+    }
 }
--- UnitOne/src/main/java/com/evgeny/unit/branching/TaskFourBranch.java
+++ UnitOne/src/main/java/com/evgeny/unit/branching/TaskFourBranch.java
@@ -1,4 +1,31 @@
 package com.evgeny.unit.branching;
 
+import java.util.Scanner;
+
 public class TaskFourBranch {
+    public static void main(String[] args) {
+        int A, B, x, y, z;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите A: ");
+        A = in.nextInt();
+        System.out.print("Введите B: ");
+        B = in.nextInt();
+        System.out.print("Введите x: ");
+        x = in.nextInt();
+        System.out.print("Введите y: ");
+        y = in.nextInt();
+        System.out.print("Введите z: ");
+        z = in.nextInt();
+        if (A > 0 && B > 0) {
+            if (x > 0 && y > 0 && z > 0) {
+                if (A >= x && B >= y || A >= y && B >= x) {
+                    System.out.println("Кирпич проходит в отверстие.");
+                } else if (A >= x && B >= z || A >= z && B >= x) {
+                    System.out.println("Кирпич проходит в отверстие.");
+                } else if (A >= z && B >= y || A >= y && B >= z) {
+                    System.out.println("Кирпич проходит в отверстие.");
+                } else System.out.println("Кирпич не проходит в отверстие.");
+            } else System.out.println("Размеры кирпича не могут быть отрицательными значениями.");
+        } else System.out.println("Отверстие не может быть с отрицательными значениями.");
+    }
 }
