9ec248e41dba43106f7f935e459964c4acb88003
Evgeny Dantsov <adriano-sport@mail.ru>
2021-09-29 15:19:07 +0300

    add task oneArray

---
+++ UnitTwo/src/main/java/com/evgeny/unit/arrayArray/TaskOne.java
@@ -0,0 +1,26 @@
+package com.evgeny.unit.arrayArray;
+
+//В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+
+import java.util.Scanner;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        int N, K, sum = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        N = in.nextInt();
+        System.out.print("Введите K: ");
+        K = in.nextInt();
+        int[] array = new int[N];
+        System.out.print("Array[" + N + "]: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 99);
+            if (array[i] % K == 0) {
+                sum = sum + array[i];
+            }
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\nСумма элементов кратных " + K + ": " + sum);
+    }
+}
---
+++ UnitTwo/src/main/java/com/evgeny/unit/arrayArray/TaskTwo.java
@@ -0,0 +1,33 @@
+package com.evgeny.unit.arrayArray;
+
+import java.util.Scanner;
+
+//Дана последовательность действительных чисел а1 ,а2 ,..., аn.
+// Заменить все ее члены, большие данного Z, этим числом. Подсчитать количество замен.
+
+public class TaskTwo
+{
+    public static void main(String[] args) {
+        int N, count = 0, Z;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер последовательности: ");
+        N = in.nextInt();
+        System.out.print("Введите Z: ");
+        Z = in.nextInt();
+        int[] array = new int[N];
+        System.out.print("Array[" + N + "]: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random() * 20);
+            System.out.print(array[i] + " ");
+        }
+        System.out.print("\nПосле замены Array[" + N + "]: ");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]>Z) {
+                count++;
+                array[i]=Z;
+            }
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\nКоличество замен: "+count);
+    }
+}
