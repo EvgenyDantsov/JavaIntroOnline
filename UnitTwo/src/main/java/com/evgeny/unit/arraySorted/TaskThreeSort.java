91b72775c5ff7c1bab4bd5889babe8c680c83b86
Evgeny Dantsov <adriano-sport@mail.ru>
2021-10-20 15:30:42 +0300

    add two task and have some mistake

--- UnitTwo/src/main/java/com/evgeny/unit/arraySorted/TaskThreeSort.java
+++ UnitTwo/src/main/java/com/evgeny/unit/arraySorted/TaskThreeSort.java
@@ -5,8 +5,43 @@ package com.evgeny.unit.arraySorted;
 //элемент и ставится на первое место, а первый - на место наибольшего. Затем, начиная со второго, эта процедура
 //повторяется. Написать алгоритм сортировки выбором.
 
+import java.util.Scanner;
+
 public class TaskThreeSort {
     public static void main(String[] args) {
+        int n, minIndex;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите натуральное число n: ");
+        n = in.nextInt();
+        System.out.print("Array: ");
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\nСортировка выбором: \nArray:");
+        for (int i = 0; i < array.length; i++) {
+            minIndex = searchMin(array, i, n - 1);
+            swap(array, i, minIndex);
+            System.out.print(array[i] + " ");
+        }
+    }
 
+    public static int searchMin(int[] array, int begin, int end) {
+        int min = array[begin];
+        int minIndex = begin;
+        for (int i = begin + 1; i <= end; i++) {
+            if (array[i] < array[i + 1]) {
+                min = array[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
--- UnitTwo/src/main/java/com/evgeny/unit/arraySorted/TaskTwoSort.java
+++ UnitTwo/src/main/java/com/evgeny/unit/arraySorted/TaskTwoSort.java
@@ -3,8 +3,45 @@ package com.evgeny.unit.arraySorted;
 //Даны две последовательности a1<=a2<=...<=an и b1<=b2<=...<=bm. Образовать из них новую последовательность
 //чисел так, чтобы она тоже была неубывающей. Примечание. Дополнительный массив не использовать.
 
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+
 public class TaskTwoSort {
     public static void main(String[] args) {
+        int n, m, i = 0, j = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите натуральное число n: ");
+        n = in.nextInt();
+        System.out.print("Введите натуральное число m: ");
+        m = in.nextInt();
+        int[] arrayOne = new int[n];
+        int[] arrayTwo = new int[m];
+        int[] arrayGeneral = new int[arrayOne.length + arrayTwo.length];
+        System.out.print("ArrayOne: ");
+        printArray(arrayOne, n);
+        System.out.print("ArrayTwo: ");
+        printArray(arrayTwo, m);
+        System.out.print("ArrayGeneral: ");
+        for (int k = 0; k < arrayGeneral.length; k++) {
+            if (j >= arrayTwo.length || i < arrayOne.length && arrayOne[i] <= arrayTwo[j]) {
+                arrayGeneral[k] = arrayOne[i];
+                i++;
+            } else if (j < arrayTwo.length) {
+                arrayGeneral[k] = arrayTwo[j];
+                j++;
+            } else {
+                arrayGeneral[k] = arrayOne[i];
+                i++;
+            }
+            System.out.print(arrayGeneral[k] + " ");
+        }
+    }
 
+    public static void printArray(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * x;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
