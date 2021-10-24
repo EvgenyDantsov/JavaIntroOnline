6029b11a8932b4409fa136f8b96165191fdbe8e7
EvgenyDantsov <adriano-sport@mail.ru>
2021-10-25 00:05:36 +0300

    add solved seven task sort

--- UnitTwo/src/main/java/com/evgeny/unit/arraySorted/TaskEightSort.java
+++ UnitTwo/src/main/java/com/evgeny/unit/arraySorted/TaskEightSort.java
@@ -3,68 +3,71 @@ package com.evgeny.unit.arraySorted;
 //Даны дроби p1/q1,p2/q2,...,pn/qn (pi, qi - натуральные). Составить программу, которая приводит эти дроби к общему
 //знаменателю и упорядочивает их в порядке возрастания.
 
+import java.util.Scanner;
+
 public class TaskEightSort {
     public static void main(String[] args) {
+        int n, generalNok;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите натуральное число n: ");
+        n = in.nextInt();
+        System.out.print("Дроби: ");
+        int[] numerator = new int[n];
+        int[] denominator = new int[n];
+        randomValue(numerator, denominator);
+        generalNok = generalNokForAllDenominators(denominator);
+        for (int i = 0; i < numerator.length; i++) {
+            numerator[i] = numerator[i] * (generalNok / denominator[i]);
+            denominator[i] = generalNok;
+        }
+        sortFraction(numerator);
+        System.out.print("Упорядоченные дроби: ");
+        for (int i = 0; i < numerator.length; i++) {
+            System.out.print(numerator[i] + "/" + denominator[i] + " ");
+        }
 
     }
+
+    public static void randomValue(int[] numerator, int[] denominator) {
+        for (int i = 0; i < numerator.length; i++) {
+            numerator[i] = (int) (Math.random() * 10);
+            denominator[i] = (int) (Math.random() * 10);
+            System.out.print(numerator[i] + "/" + denominator[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int nod(int a, int b) {
+        return b == 0 ? a : nod(b, a % b);
+    }
+
+    public static int nok(int a, int b) {
+        return a * b / nod(a, b);
+    }
+
+    public static int generalNokForAllDenominators(int[] denominator) {
+        int b = 0;
+        for (int i = 0; i < denominator.length - 1; i++) {
+            if (b == 0) {
+                b = nok(denominator[i], denominator[i + 1]);
+            } else {
+                b = nok(b, denominator[i + 1]);
+            }
+        }
+        return b;
+    }
+
+    public static void sortFraction(int[] numerator) {
+        int temp;
+        for (int i = 0; i < numerator.length; i++) {
+            for (int j = i + 1; j < numerator.length; j++) {
+                if (numerator[i] > numerator[j]) {
+                    temp = numerator[j];
+                    numerator[j] = numerator[i];
+                    numerator[i] = temp;
+                }
+            }
+        }
+    }
 }
-//    public static void eighthTask(int[] arr1, int[] arr2) {
-//        System.out.printf("%nAnswer eighthTask: %n");
-//        System.out.print("Числители:   ");
-//        printIntArray(arr1);
-//        System.out.print("Знаменатели: ");
-//        printIntArray(arr2);
-//
-//        int max = arr2[0];
-//
-//        for (int i = 1; i < arr2.length; i++) {
-//            if (max < arr2[i]) {
-//                max = arr2[i];
-//            }
-//        }
-//        int k = 2;
-//        for (int i = 0; i < arr2.length; i++) {
-//            if (max % arr2[i] != 0) {
-//                max = max * k;
-//                k++;
-//                i = 0;
-//            }
-//        }
-//        for (int i = 0; i < arr1.length; i++) {
-//            arr1[i] = arr1[i] * (max / arr2[i]);
-//            arr2[i] = max;
-//        }
-//
-//        System.out.println("Общий знаменатель равен: " + max);
-//        sortIntegerArray(arr1);
-//        System.out.print("Числители:   ");
-//        printIntArray(arr1);
-//        System.out.print("Знаменатели: ");
-//        printIntArray(arr2);
-//    }
-//    public static void sortIntegerArray(int[] arr) {
-//
-//        int max;
-//        int temp;
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            max = i;
-//            for (int j = i; j < arr.length; j++) {
-//                if (arr[max] < arr[j]) {
-//                    max = j;
-//                }
-//            }
-//            temp = arr[i];
-//            arr[i] = arr[max];
-//            arr[max] = temp;
-//        }
-//
-//    }
-//    static void printIntArray(int[] arr) {
-//        for (int elem : arr) {
-//            System.out.print(elem + " ");
-//        }
-//        System.out.println();
-//    }
-//
-//}
+
