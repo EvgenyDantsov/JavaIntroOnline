731ff57c10ae439b8415b45cfc2d1e74ef79759e
Evgeny Dantsov <adriano-sport@mail.ru>
2021-11-01 14:47:08 +0300

    add sloved 16-17 task to decomposition

--- UnitTwo/src/main/java/com/evgeny/unit/decompositionMethods/TaskSeventeenDecomposition.java
+++ UnitTwo/src/main/java/com/evgeny/unit/decompositionMethods/TaskSeventeenDecomposition.java
@@ -4,8 +4,33 @@ package com.evgeny.unit.decompositionMethods;
 // Из результата вновь вычли сумму его цифр и т.д. Сколько таких действий надо произвести,
 // чтобы получился нуль? Для решения задачи использовать декомпозицию.
 
+import java.util.Scanner;
+
 public class TaskSeventeenDecomposition {
     public static void main(String[] args) {
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число n: ");
+        n = in.nextInt();
+        searchNumberOfAction(n);
+    }
 
+    public static int sumDigitNumber(int number) {
+        int sum = 0, temp;
+        while (number > 0) {
+            temp = number % 10;
+            sum = sum + temp;
+            number = number / 10;
+        }
+        return sum;
+    }
+
+    public static void searchNumberOfAction(int n) {
+        int count = 0;
+        while (n > 0) {
+            n = n - sumDigitNumber(n);
+            count++;
+        }
+        System.out.println("Количество операций необходимо, чтобы получился ноль: " + count);
     }
 }
--- UnitTwo/src/main/java/com/evgeny/unit/decompositionMethods/TaskSixteenDecomposition.java
+++ UnitTwo/src/main/java/com/evgeny/unit/decompositionMethods/TaskSixteenDecomposition.java
@@ -3,8 +3,50 @@ package com.evgeny.unit.decompositionMethods;
 //Написать программу, определяющую сумму n - значных чисел, содержащих только нечетные цифры.
 // Определить также, сколько четных цифр в найденной сумме. Для решения задачи использовать декомпозицию.
 
+import java.util.Scanner;
+
 public class TaskSixteenDecomposition {
     public static void main(String[] args) {
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите натуральное число от 1 до 9, n: ");
+        n = in.nextInt();
+        if (n > 0 && n < 9) {
+            sumOddNumber(n);
+        } else System.out.println("Введены некорректные данные.");
+    }
 
+    public static boolean searchOddNumber(int number) {
+        int temp;
+        while (number != 0) {
+            temp = number % 10;
+            if (temp % 2 != 0) {
+                number = number / 10;
+            } else return false;
+        }
+        return true;
+    }
+
+    public static void sumOddNumber(int n) {
+        int sum = 0;
+        for (int i = (int) Math.pow(10, n - 1); i < Math.pow(10, n); i++) {
+            if (searchOddNumber(i)) {
+                sum = sum + i;
+            }
+        }
+        System.out.println("Сумма " + n + "-значных чисел, содержащих только нечетные числа: " + sum);
+        searchEvenNumberToSum(sum);
+    }
+
+    public static void searchEvenNumberToSum(int number) {
+        int temp, count = 0;
+        while (number > 0) {
+            temp = number % 10;
+            if (temp % 2 == 0 && temp != 0) {
+                count++;
+            }
+            number = number / 10;
+        }
+        System.out.println("Количество четных чисел в сумме: " + count);
     }
 }
