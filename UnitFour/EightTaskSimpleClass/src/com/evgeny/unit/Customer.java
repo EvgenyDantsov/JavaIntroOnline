0b9043171f7327ecdce89fbdcf3f450f4f4c5746
EvgenyDantsov <adriano-sport@mail.ru>
2021-12-05 22:11:01 +0300

    add solved 8 task simpleClass

--- UnitFour/EightTaskSimpleClass/src/com/evgeny/unit/Customer.java
+++ UnitFour/EightTaskSimpleClass/src/com/evgeny/unit/Customer.java
@@ -1,6 +1,118 @@
 package com.evgeny.unit;
 
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Customer {
-    private int id, numberCreditCard, numberBankAccount;
-    private String name, surname, patronymic, address;
+    private static int idGenerator;
+    private int id;
+    private String name, surname, patronymic, address, numberCreditCard, numberBankAccount;
+
+    public Customer(String name, String surname, String patronymic, String address,
+                    String numberCreditCard, String numberBankAccount) {
+        id = idGenerator++;
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.address = address;
+        this.numberCreditCard = numberCreditCard;
+        this.numberBankAccount = numberBankAccount;
+    }
+
+    public void print() {
+        System.out.println(toString());
+    }
+
+    public String getNumberBankAccount() {
+        return numberBankAccount;
+    }
+
+    public void setNumberBankAccount(String numberBankAccount) {
+        this.numberBankAccount = numberBankAccount;
+    }
+
+    public String getNumberCreditCard() {
+        return numberCreditCard;
+    }
+
+    public void setNumberCreditCard(String numberCreditCard) {
+        this.numberCreditCard = numberCreditCard;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static Customer getCustomer(Scanner scanner) {
+        Customer customer = new Customer("", "", "", "", "", "");
+        System.out.print("Фамилия: ");
+        customer.surname = scanner.next();
+        System.out.print("Имя: ");
+        customer.name = scanner.next();
+        System.out.print("Отчесто: ");
+        customer.patronymic = scanner.next();
+        scanner.nextLine();
+        System.out.print("Адрес: ");
+        customer.address = scanner.nextLine();
+        System.out.print("Номер кредитной карты: ");
+        customer.numberCreditCard = scanner.next();
+        System.out.print("Номер банковского счета: ");
+        customer.numberBankAccount = scanner.next();
+        return customer;
+    }
+
+    public static class ByNameComparator implements Comparator<Customer> {
+        @Override
+        public int compare(Customer left, Customer right) {
+            if (left.surname != right.surname)
+                return left.surname.compareTo(right.surname);
+            if (left.name != right.name)
+                return left.name.compareTo(right.name);
+            return left.patronymic.compareTo(right.patronymic);
+        }
+    }
+
+    public String toString() {
+        String string = "";
+        string = string + id + ". ФИО: " + name + " " + surname + " " + patronymic +
+                " Address: " + address + " numberCreditCard: " + numberCreditCard + " " +
+                "numberBankAccount: " + numberBankAccount;
+        return string;
+    }
 }
--- UnitFour/EightTaskSimpleClass/src/com/evgeny/unit/Main.java
+++ UnitFour/EightTaskSimpleClass/src/com/evgeny/unit/Main.java
@@ -9,9 +9,44 @@ package com.evgeny.unit;
 //        a) список покупателей в алфавитном порядке;
 //        b) список покупателей, у которых номер кредитной карточки находится в заданном интервале
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        Customers customers = new Customers(1);
+        while (true) {
+            System.out.print(
+                    "Выберете пункт меню:\n" +
+                            "0. Выйти\n" +
+                            "1. Добавить покупателя\n" +
+                            "2. Вывод покупателей в алфавитном порядке\n" +
+                            "3. Вывод покупателей с номером кредитной картой в заданном интервале\n" +
+                            "Введите номер пункта меню: ");
+            int choice = in.nextInt();
+            if (choice == 0)
+                break;
+            if (choice < 1 || choice > 3) {
+                System.out.println("выбран неправильный пункт меню, повторите ввод.");
+                continue;
+            }
+            switch (choice) {
+                case 1:
+                    customers.pushBack(Customer.getCustomer(in));
+                    break;
+                case 2:
+                    customers.sortByName();
+                    customers.print();
+                    break;
+                case 3:
+                    System.out.println("Введи диапазон кредитных карт\n");
+                    System.out.print("От: ");
+                    String from = in.next();
+                    System.out.print("До: ");
+                    String to = in.next();
+                    customers.printIfCardIn(from, to);
+            }
+        }
     }
 }
