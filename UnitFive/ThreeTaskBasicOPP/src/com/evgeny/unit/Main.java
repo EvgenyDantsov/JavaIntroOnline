bb6a6c0c73cabce67ee4d7f88217c8830a615d7b
Evgeny Dantsov <adriano-sport@mail.ru>
2022-01-03 14:51:56 +0300

    add tasks five unit in BasicOPP

---
+++ UnitFive/ThreeTaskBasicOPP/src/com/evgeny/unit/Main.java
@@ -0,0 +1,11 @@
+package com.evgeny.unit;
+
+//Создать класс Календарь с внутренним классом, с помощью объектов которого
+// можно хранить информацию о выходных и праздничных днях.
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ UnitFive/TwoTaskBasicOPP/src/com/evgeny/unit/Main.java
@@ -0,0 +1,11 @@
+package com.evgeny.unit;
+
+//Создать класс Payment с внутренним классом, с помощью объектов
+// которого можно сформировать покупку из нескольких товаров.
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
